5ddbc4e42a25169b075960236ffab93423b042d2
hkoh1024 <hkoh1024@day1ent.com>
2023-03-28 18:22:43 +0900

    feat initDocker

--- initDocker.sh
+++ initDocker.sh
@@ -16,10 +16,12 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt-get isntall docker-compose
 
 sudo bash ./generateKeyfile.sh
