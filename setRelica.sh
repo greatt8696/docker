71cfc5cf2cfbf3f47c8e95282d4990925aac1e56
hkoh1024 <hkoh1024@day1ent.com>
2023-03-29 14:01:05 +0900

    123

--- setRelica.sh
+++ setRelica.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo docker exec -i mongo1 /usr/bin/mongosh <<EOF 
-rs.initiate({ _id: "myReplicaSet", members: [ {_id: 0, host: "mongo1"}, {_id: 1, host: "mongo2"}, {_id: 2, host: "mongo3"}]});
+sudo docker exec -i mongo1 mongosh <<EOF 
 EOF
 
+rs.initiate({ _id: "myReplicaSet", members: [ {_id: 0, host: "mongo1"}, {_id: 1, host: "mongo2"}, {_id: 2, host: "mongo3"}]});
 
 
