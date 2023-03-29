8a9eed1defb5eda8fd64548eeaf1c4b82387aafc
hkoh1024 <hkoh1024@day1ent.com>
2023-03-29 13:47:23 +0900

    feat setReplica

--- setRelica.sh
+++ setRelica.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-sudo docker exec -t
+rs.initiate({ _id: "myReplicaSet", members: [ {_id: 0, host: "mongo1"}, {_id: 1, host: "mongo2"}, {_id: 2, host: "mongo3"}]});
+sudo docker exec -it "/usr/bin/mongosh "
+
+
