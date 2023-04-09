abca54cf6a5a4841fa31da97463209ad277bf730
greatt8696 <greatt8696@gmail.com>
2023-04-09 22:00:13 +0900

    `12

--- mongodb/setReplica.sh
+++ mongodb/setReplica.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo docker exec -i gameDb1 mongosh -u local -p haru <<EOF 
-rs.initiate({ _id: "gameDbReplicaSet", members: [ {_id: 0, host: "gameDb1"}, {_id: 1, host: "gameDb2"}, {_id: 2, host: "gameDb3"}]});
+sudo docker exec -i gamedb1 mongosh -u local -p haru <<EOF 
+rs.initiate({ _id: "gamedbReplicaset", members: [ {_id: 0, host: "gamedb1"}, {_id: 1, host: "gamedb2"}, {_id: 2, host: "gamedb3"}]});
 EOF
 
 
