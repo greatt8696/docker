f95c5fe1180727fb5dde2effb3d65f3182346f9a
greatt8696 <greatt8696@gmail.com>
2023-04-27 18:57:17 +0900

    123

--- mongodb/generateKeyfile_Mac.sh
+++ mongodb/generateKeyfile_Mac.sh
@@ -1,3 +1,3 @@
-openssl rand -base64 756 > ./mongodb.key
-chmod 600 ./mongodb.key
-chown 999:999 ./mongodb.key
+openssl rand -base64 756 > ../mongodb.key
+chmod 600 ../mongodb.key
+chown 999:999 ../mongodb.key
